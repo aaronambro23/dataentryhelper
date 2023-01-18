bbda000e1a13cdb944f5b2c371039f316d2fc9bd
aaronambro23 <aaronambro23@gmail.com>
2023-01-18 22:47:18 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+function Header() {
+  return <h1>Data Entry Helper</h1>;
+}
+
+const Description = () => {
+  return <p>Upload a photo and our data entry helper will extract the text you need</p>;
+}
+
+ function UploadForm() {
+  const [file, setFile] = useState(null);
+  
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(file);
+    // handle file upload
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleChange} />
+      <button type="submit">Upload Photo</button>
+    </form>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Description />
+      <br />
+      <UploadForm />
     </div>
   );
 }
